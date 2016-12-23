549e897af024f02e903ffc32eac3e9436638b534
panyawei <panywa@163.com>
2016-12-23 09:56:19 +0800

    this is the DefaultParameter study code

---
+++ DefaultParameter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      潘先生
+#
+# Created:     21/12/2016
+# Copyright:   (c) 潘先生 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+def say(message, times = 1):
+    print message * times
+say('Hello')
+say('World', 5)
+
+
