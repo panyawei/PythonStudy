da7d57d70ff3be0db4a02d3fac57900947457ebb
panyawei <panywa@163.com>
2016-12-23 08:42:24 +0800

    This is the dict study code

---
+++ using_dict.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      潘先生
+#
+# Created:     21/12/2016
+# Copyright:   (c) 潘先生 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    ab = { 'Swaroop' : 'swaroopch@byteofpython.info',
+           'Larry' : 'larry@wall.org',
+           'Matsumoto' : 'matz@ruby-lang.org',
+           'Spammer' : 'spammer@hotmail.com'
+         }
+    print "Swaroop's address is %s" % ab['Swaroop']   # return value
+    # append
+    ab['Guido'] = 'guido@python.org'
+    print ab
+    # delete
+    del ab['Spammer']
+    print ab
+    # traverse
+    for name, address in ab.items():
+        print 'Contact %s at %s' % (name, address)
+
+if __name__ == '__main__':
+    main()
