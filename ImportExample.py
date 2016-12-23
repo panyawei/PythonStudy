5e7736f7feccde86adced879078dc88da839feed
panyawei <panywa@163.com>
2016-12-23 09:49:22 +0800

    this is the example of Import

---
+++ ImportExample.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      潘先生
+#
+# Created:     21/12/2016
+# Copyright:   (c) 潘先生 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+printString="Hello,Python!"
+
+def say():
+    print 'hello'
+say()
+
+if __name__ == '__main__':
+    print 'This program is being run by itself'
+else:
+    print 'I am being imported from another module'
