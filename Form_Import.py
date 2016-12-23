7fac3e5d5ecf8548cca0a2ffd4d601aa906286fb
panyawei <panywa@163.com>
2016-12-23 09:41:37 +0800

    this is the Form_Import study code

---
+++ Form_Import.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      潘先生
+#
+# Created:     21/12/2016
+# Copyright:   (c) 潘先生 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from module import say,printString
+def main():
+    say()
+    print printString
+
+if __name__ == '__main__':
+    main()
