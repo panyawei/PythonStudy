07b60ae2ca22b40b18cab69fe32974aa3a13b2c4
panyawei <panywa2163.com>
2017-05-11 14:28:58 +0800

    爬取ChinaUnix论坛,使用cookie

--- reptile_re_chinaUnix_cookie.py
+++ reptile_re_chinaUnix_cookie.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from urllib import urlencode
 from urllib2 import Request,urlopen,build_opener,HTTPCookieProcessor,install_opener
+import cookielib
 url="http://bbs.chinaunix.net/member.php?mod=logging@action=login@loginsubmit" \
     "=yes@loginhash=L768q"
 postdata=urlencode({
@@ -10,17 +11,22 @@ postdata=urlencode({
 req=Request(url,postdata)
 req.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                             "(KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393")
+# 获取Cookiejar对象（存在本机的cookie消息）
+clib=cookielib.CookieJar()
 
-data=urlopen(req).read()
-handle=open("G://e1.html","wb")
-handle.write(data)
-handle.close()
+# 创建cookie处理器，并构建opener对象
+opener=build_opener(HTTPCookieProcessor(clib))
+# 将opener设置为全局
+install_opener(opener)
+file=opener.open(req)
+data=file.read()
+file=open('G://f1.html','wb')
+file.write(data)
+file.close()
+
 
 url2="http://bbs.chunaunix.net/"
-req2=Request(url2,postdata)
-req2.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                            "(KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393")
-data2=urlopen(req2).read()
-handle2=open('G://e2.html',"wb")
+data2=urlopen(url2).read()
+handle2=open('G://f2.html',"wb")
 handle2.write(data2)
 handle2.close()
